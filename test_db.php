f0f71a34265dd89af1d5cab9e01f334061a13262
tieunico <43010384+tieunico@users.noreply.github.com>
2018-09-06 11:52:44 -0500

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -3,12 +3,24 @@
     $username="root";
     $password="";
     $db="phpmyadmin";
-
+    
+    //create connection for database
     $db= new mysqli($servername,$username,$password,$db);
-
+    //check connection
     if (mysqli_connect_error()){
       die ("Database connection failed: ". mysqli_connect_error());
     }
     echo "Connect successfully";
+    
+    //create a new database name "testDB"
+    /*
+    $sql="CREATE DATABASE testDB";
+    if ($conn->query($sql)===TRUE){
+        echo "Database created successfully!";
+    }
+    else{
+        echo "Failed to create database";
+    }
+    */
 
  ?>
